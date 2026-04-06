938375e8860dbc610aacf21e4045eb5f2527c9d1
Rasulbek Xakimbayev <rasulbekxakimbayev@gmail.com>
2026-04-06 16:09:10 +0500

    Add files via upload

---
+++ SQL/db.py
@@ -0,0 +1,14 @@
+import sqlite3 as sq
+
+con = sq.connect("database.db")
+cur = con.cursor()
+
+cur.execute(""" CREATE TABLE IF NOT EXISTS users(
+    id INTEGER,
+    jins INTEGER,
+    name TEXT,
+    level INTEGER,
+    score INTEGER
+            )""")
+
+con.colse
